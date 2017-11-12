1a9f52b0ed6af4d41178acee62a27555ba527176
roba <achujel3@gmail.com>
2017-11-12 14:59:25 +0200

    Testing some constants

---
+++ src/io/tutorialspoint/_05_testing_some_constants/run/Main.java
@@ -0,0 +1,17 @@
+package io.tutorialspoint._05_testing_some_constants.run;
+
+public class Main {
+
+    public static final String RANDOM = "random";
+    public static final String GENERAL = "general";
+    public static final String MEDIUM = "medium";
+
+    public static void main(String[] args) {
+
+        System.out.println(Main.GENERAL);
+        System.out.println(Main.RANDOM);
+        System.out.println(Main.MEDIUM);
+
+    }
+
+}
